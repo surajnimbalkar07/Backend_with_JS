4195a580e55b503e8a3f3c600363de9aeceba936
surajnimbalkar07 <136218136+surajnimbalkar07@users.noreply.github.com>
2025-11-24 13:21:22 +0530

    started with controller.routes

--- src/app.js
+++ src/app.js
@@ -23,3 +23,10 @@ app.use(express.urlencoded({extended:true}))
 // extended: true	supports nested objects & arrays
 app.use(cookieParser())
 app.use(express.static("public"))
+
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+//route declaration
+app.use("/api/v1/users",userRouter)
